513f7dbe94d72c7585cf74fcb450c78091395726
rezarria <62886905+rezarria@users.noreply.github.com>
2020-11-23 12:33:56 +0700

    up

--- thuc hanh/TIẾT 2 - xong/bai11.c
+++ thuc hanh/TIẾT 2 - xong/bai11.c
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-void sort(uint32_t i, uint64_t n);
+void sort(uint32_t* i, uint64_t n);
 
 int main()
 {
@@ -14,17 +14,34 @@ int main()
 
 	for (uint64_t i = 0llu; i < n; i++)
 	{
-		printf("Nhap phan tu %llu\t:\t");
+		printf("Nhap phan tu %llu\t:\t", i);
 		scanf("%d", &arr[i]);
 	}
 
 	sort(arr, n);
 
-	printf("Nhap phan tu x\t:\t");
+
+	for (uint64_t i = 0llu; i < n; i++)
+		printf("%5d", arr[i]);
+
+
+	printf("\nNhap phan tu x\t:\t");
 	scanf("%d", &arr[n]);
 	n++;
 
-	sort(arr, n);
+
+
+
+	if (!(arr[n - 2llu] < arr[n - 1llu]))
+		if (arr[0] > arr[n - 1llu])
+			for (uint64_t i = n - 1; i > 0llu; i--)
+			{
+				arr[i] = arr[i] ^ arr[i - 1llu];
+				arr[i - 1llu] = arr[i] ^ arr[i - 1llu];
+				arr[i] = arr[i] ^ arr[i - 1llu];
+			}
+		else
+			sort(arr, n);
 
 	for (uint64_t i = 0llu; i < n; i++)
 		printf("%5d", arr[i]);
@@ -35,12 +52,13 @@ int main()
 
 void sort(uint32_t* arr, uint64_t n)
 {
-	for (uint64_t i < 0; i < n - 1llu; i++)
+	for (uint64_t i = 0; i < n - 1llu; i++)
 		for (uint64_t j = i + 1llu; j < n; j++)
 			if (arr[i] > arr[j])
 			{
-				i = i ^ j;
-				j = i ^ j;
-				i = i ^ j;
+				arr[i] = arr[i] ^ arr[j];
+				arr[j] = arr[i] ^ arr[j];
+				arr[i] = arr[i] ^ arr[j];
+
 			}
 }
--- thuc hanh/TIẾT 2 - xong/bai12.c
+++ thuc hanh/TIẾT 2 - xong/bai12.c
@@ -5,30 +5,32 @@ int main()
 	printf("Nhap n = ");
 	scanf("%d", &n);
 	int arr[2][n];
-	for(char t = 0; t < 2; t++)
+	for (char t = 0; t < 2; t++)
 	{
 		printf("Nhap mang %c :\n", 'a' + t);
-		for(int i = 0; i < n; i++)
+		for (int i = 0; i < n; i++)
 		{
 			printf("Nhap phan tu %d : ", i);
 			scanf("%d", &arr[t][i]);
 		}
-		for(int i = 0; i < n - 1; i++)
-			for(int j = i + 1; j < n; j++)
+		for (int i = 0; i < n - 1; i++)
+			for (int j = i + 1; j < n; j++)
 			{
 				arr[t][i] += arr[t][j];
 				arr[t][j] = arr[t][i] - arr[t][j];
 				arr[t][i] -= arr[t][j];
 			}
 	}
+
 	char check = 0;
-	for(int i = 0; i < n; i++)
-		if(arr[0][i] != arr[1][i])
+	for (int i = 0; i < n; i++)
+		if (arr[0][i] != arr[1][i])
 		{
 			check++;
 			break;
 		}
-	if(check)
+
+	if (check)
 		printf("\nHai mang khong tuong dong\n");
 	else
 		printf("\nHai mang tuong dong\n");
