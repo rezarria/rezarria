e7113e7da619a219818ed3ebfd2d1edbf5a3a237
rezarria <votanamcb@outlook.com>
2021-01-28 19:04:26 +0700

    1

--- dc/1/1.c
+++ dc/1/1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+float max(float a, float b)
+{
+    return (a > b) ? a : b;
+}
+
+float max3(float a, float b, float c)
+{
+    return max(a, max(b, c));
+}
+
 int main()
 {
+    float a, b, c;
+    printf("nhap a, b ,c : ");
+    scanf("%f%f%f", &a, &b, &c);
+    printf("So lon nhat trong 3 so : %f\n", max3(a, b, c));
     return 0;
 }
