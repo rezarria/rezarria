f5fa35e7dae35fddc764e25817089f2a2f8934ae
rezarria <votanamcb@outlook.com>
2021-01-28 20:07:27 +0700

    7

---
+++ dc/1/7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool hoanThien(unsigned int n)
+{
+    unsigned int t = 1;
+    unsigned int moc = sqrt(n);
+    for (; moc > 1; moc--)
+        if (!(n % moc))
+            t += moc + n / moc;
+    return n == t;
+}
+
+int main()
+{
+    unsigned int n;
+    printf("Nhap so : ");
+    scanf("%u", &n);
+    if (hoanThien(n))
+        printf("La so hoan thien");
+    else
+        printf("Khong phai la so khong hoan thien");
+    return 0;
+}
