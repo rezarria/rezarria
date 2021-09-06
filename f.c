b807257fb1bfc1ed87638b554d282d5ff59844c5
rezarria <62886905+rezarria@users.noreply.github.com>
2021-09-06 18:34:02 +0700

    Update f.c

--- f.c
+++ f.c
@@ -8,19 +8,22 @@ int main(int argc, string argv[]) //int or string kv
 {
     if (argc == 2)
     {
-        int i = 0, j;
         int length = strlen(argv[1]);
 
-        for (; i < length - 1; ++i)
-            for (j = i + 1; j < length; ++j)
-                if (tolower(argv[1][i]) == tolower(argv[1][j]))
-                {
-                    i = length; // đặt i và j = length để hủy toàn bộ 2 vòng lặp
-                    j = length;
-                    break;
-                }
+        if (length == 26) // kiểm tra đủ 26 kí tự
+        {
+            int i = 0, j;
 
-        /*
+            for (; i < length - 1; ++i)
+                for (j = i + 1; j < length; ++j)
+                    if (tolower(argv[1][i]) == tolower(argv[1][j]))
+                    {
+                        i = length; // đặt i và j = length để hủy toàn bộ 2 vòng lặp
+                        j = length;
+                        break;
+                    }
+
+            /*
             nếu i và j chạy hết vòng thử và không có trùng kí tự
             thì j == length và i == length - 1
             tức j và i không bằng nhau
@@ -29,27 +32,28 @@ int main(int argc, string argv[]) //int or string kv
             hoặc
             i + 1 == j (hoặc i == j - 1)
         */
-        if (j != i)
-        {
-            char *str = get_string("plaintext:  ");
-
-            for (i = 0; str[i] != '\0'; ++i) // xâu luôn được kết thúc bởi kí tự '\0'
+            if (j != i)
             {
-                if (isalpha(str[i]))
+                char *str = get_string("plaintext:  ");
+
+                for (i = 0; str[i] != '\0'; ++i) // xâu luôn được kết thúc bởi kí tự '\0'
                 {
-                    if (islower(str[i]))
+                    if (isalpha(str[i]))
                     {
-                        str[i] = tolower(argv[1][str[i] - 97]);
-                    }
-                    else
-                    {
-                        str[i] = toupper(argv[1][str[i] - 65]);
+                        if (islower(str[i]))
+                        {
+                            str[i] = tolower(argv[1][str[i] - 97]);
+                        }
+                        else
+                        {
+                            str[i] = toupper(argv[1][str[i] - 65]);
+                        }
                     }
                 }
-            }
 
-            printf("ciphertext: %s", str);
-            return 0;
+                printf("ciphertext: %s", str);
+                return 0;
+            }
         }
 
         // Xảy ra khi không phát hiện trùng
